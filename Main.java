5026340536f4efe7be7e31d6d355ae368644eb07
Aiman Kayad <Akaya005@fiu.edu>
2022-06-01 08:35:47 -0400

    Correct typo in solution explanation
    Correct typo in solution explanation

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ class Main {
     - Create a char array, and use str.toCharArray(); to fill it with characters from our String.
     - initialize two integer variables to serve as pointers, one to represent the first index of the char array, and one to represent the last index of the char array.
     - Create a while loop that runs until (start < end).
-    - swap the first and last values of the char array at each step until (string >= end).
+    - swap the first and last values of the char array at each step until (start >= end).
     - Outside of the while loop, create a new String and give it the result of our operation by using String.valueOf(arr);
     - return the created String.
 
